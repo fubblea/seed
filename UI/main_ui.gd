2af688d3f72c496f14fdeb0f9b52cc0be2ded05e
fubblea <ajay.arthur@gmail.com>
2025-07-29 21:41:51 -0400

    Created sliders for water and sunlight

---
+++ UI/main_ui.gd
@@ -0,0 +1,8 @@
+extends CanvasLayer
+
+func _on_water_slider_value_changed(value: float) -> void:
+	print("Water slider value changed to: ", value)
+
+
+func _on_sunlight_slider_value_changed(value: float) -> void:
+	print("Sunlight slider value changed to: ", value)
